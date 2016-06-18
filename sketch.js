45288f8a200bd6809b18f07d9c42399e1ccbce45
Jack Tregurtha <jacktregurtha@gmail.com>
2016-06-18 17:13:14 -0400

    Planets branch first commit

--- sketch.js
+++ sketch.js
@@ -1,22 +1,26 @@
 var walk;
 var walkers = [];
-var numWalkers = 60;
+var numWalkers = 2;
 var padding = 100;
+
 
 function setup() {
   createCanvas(screen.width,screen.height);
   for(var i = 0; i < numWalkers; i++) {
     walk = new Walker();
     walkers.push(walk);
+
   }
-  
 }
 
 function draw() {
+  // if walkers[0].getXPosition() > width {
+  //   return;
+  // }
 
   push();
   for (var i = 0; i < numWalkers; i++) {
-    translate(0, (height-padding)/numWalkers);
+    translate(0, 50);
     walkers[i].update();
     walkers[i].display();
 
@@ -32,11 +36,11 @@ function border() {
    rect(width - padding ,0, padding, height);
    rect(0,0,width, padding)
       rect(0,height-padding,width, padding)
-  
+
 }
 
 function mouseReleased() {
 
   fullscreen(true);
-  
+
 }
